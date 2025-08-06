20b1ce4d0cf7478876846b2683d15ac64d5007bf
Sockford2210 <r.jsyoung97@gmail.com>
2025-08-06 10:41:20 +0100

    removed unused test.py

--- solutions/Test.py
+++
@@ -1,6 +0,0 @@
-from Directions import build_directions, DIAGONAL_DIRECTIONS
-
-directions = build_directions(DIAGONAL_DIRECTIONS)
-
-for direction in directions:
-    print(direction.name + ": {" + str(direction.x) + "," + str(direction.y) + "}, length: " + str(direction.get_length()))
